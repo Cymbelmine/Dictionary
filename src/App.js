88db8a3c2691e7883be907b408bdaab8cc00b62c
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-04-27 13:27:50 -0500

    Import Logo
    - import logo to App.js
    - edit logo in App.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import logo from "./logo.png";
 import './App.css';
 
 function App() {
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
     
         <header className="App-header">
-        
+        <img src={logo} className="App-logo img-fluid" 
+        alt="logo" />
        
       </header>
       Hello
