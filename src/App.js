8027030cd18f654f8fc9d0068d868373e2793d39
Cymbelmine <74141947+Cymbelmine@users.noreply.github.com>
2021-04-28 14:47:11 -0500

    Created the Dictionary component
    The Dictionary component in Dictionary.js contains a search function.

--- src/App.js
+++ src/App.js
@@ -13,8 +13,10 @@ export default function App() {
       <main>
         <Dictionary />
       </main>
-      <footer className="text-center">
+      <footer className="App-footer">
+        <small>
         Coded by SheCodes 
+        </small>
       </footer>
       </div>
     </div>
